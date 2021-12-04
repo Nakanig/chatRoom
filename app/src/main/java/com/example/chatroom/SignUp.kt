946402eac82916fa52433fdd36396527f46e1ed6
Nakanig <giorgi.nakani.1@btu.edu.ge>
2021-12-04 21:46:22 +0400

    nakani

--- app/src/main/java/com/example/chatroom/SignUp.kt
+++ app/src/main/java/com/example/chatroom/SignUp.kt
@@ -16,6 +16,7 @@ class SignUp : AppCompatActivity() {
     private lateinit var edtName: EditText
     private lateinit var edtEmail: EditText
     private lateinit var edtPassword: EditText
+    private lateinit var edtConfirmPassword: EditText
     private lateinit var btnSignUp: Button
     private lateinit var mAuth: FirebaseAuth
     private lateinit var mDbRef: DatabaseReference
@@ -34,34 +35,51 @@ class SignUp : AppCompatActivity() {
         edtName = findViewById(R.id.edt_name)
         edtEmail = findViewById(R.id.edt_email)
         edtPassword = findViewById(R.id.edt_password)
+        edtConfirmPassword = findViewById(R.id.edt_confirmpassword)
         btnSignUp = findViewById(R.id.btnSignup)
 
         btnSignUp.setOnClickListener {
             val name = edtName.text.toString()
             val email = edtEmail.text.toString()
             val password = edtPassword.text.toString()
+            val confirmpassword = edtConfirmPassword.text.toString()
             
             if (name.isEmpty()){
                 edtName.error = "Aucilebelia velis shevseba"
                 return@setOnClickListener
-            }else if (email.isEmpty()){
-                edtEmail.error = "Aucilebelia velis shevseba"
+            }else if (email.isEmpty() && !email.contains("@")){
+                edtEmail.error = "Aucilebelia  velis validuri email-it shevseba"
                 return@setOnClickListener
-            }else if (password.isEmpty()){
+            }else if (password.isEmpty()) {
                 edtPassword.error = "Aucilebelia velis shevseba"
+                return@setOnClickListener
+            }else if (password.length <= (9)) {
+                Toast.makeText(this, "Paroli unda sheicavdes 9 simbolos", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }else if (!password.contains("1") and !password.contains("2") and !password.contains("3")
+                and !password.contains("4") and !password.contains("5") and !password.contains("6")
+                and !password.contains("7") and !password.contains("8") and !password.contains("9")
+                and !password.contains("0")){
+                Toast.makeText(this, "Paroli unda sheicavdes min 1 cifrs", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }else if (confirmpassword.isEmpty()) {
+                edtConfirmPassword.error = "Aucilebelia velis shevseba"
+                return@setOnClickListener
+            }else if (confirmpassword != password) {
+                Toast.makeText(this, "Aucilebelia parolebi emtxveodes", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }else{
                 Toast.makeText(this, "LOADING...", Toast.LENGTH_SHORT).show()
             }
             
 
-            signup(name,email,password)
+            signup(name,email,password,confirmpassword)
         }
 
     }
 
-    private fun signup(name:String, email: String, password: String){
-        // user gaketebis logika
+    private fun signup(name:String, email: String, password: String, confirmpassword: String){
+        // user sheqmnis logika
 
         mAuth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
@@ -75,7 +93,7 @@ class SignUp : AppCompatActivity() {
 
 
                 }else {
-                    Toast.makeText(this@SignUp, "Error Occurred", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this@SignUp, "Error Occured", Toast.LENGTH_SHORT).show()
                 }
             }
     }
